2e04f51da66b2aadca9a0a6bc5a597da37aea95f
metulburr <micahpage911@yahoo.com>
2014-12-09 03:28:56 -0500

    added cli args, started craps section

--- data/prepare.py
+++ data/prepare.py
@@ -1,11 +1,9 @@
 """
 This module initializes the display and creates dictionaries of resources.
 """
-
 import os
 import pygame as pg
 from . import tools
-
 
 ORIGINAL_CAPTION = "Py Roller Casino"
 START_SIZE = (1024, 768)
@@ -13,17 +11,26 @@ RENDER_SIZE = (1400, 1050)
 RESOLUTIONS = [(800, 600), (1024, 768), (1280, 960), (1400, 1050)]
 CARD_SIZE = (125, 181)
 CHIP_SIZE = (32, 32)
+WIN_POS = (0,0)
+ARGS = tools.get_cli_args(ORIGINAL_CAPTION, WIN_POS, START_SIZE)
+#adjust settings based on args
+START_SIZE = int(ARGS['size'][0]), int(ARGS['size'][1])
 
 #Pre-initialize the mixer for less delay before a sound plays
 pg.mixer.pre_init(44100, -16, 1, 512)
 
-
 #Initialization
 pg.init()
-os.environ['SDL_VIDEO_CENTERED'] = "TRUE"
+if ARGS['center']:
+    os.environ['SDL_VIDEO_CENTERED'] = "True"
+else:
+    os.environ['SDL_VIDEO_WINDOW_POS'] = '{},{}'.format(ARGS['winpos'][0], ARGS['winpos'][1])
 pg.display.set_caption(ORIGINAL_CAPTION)
-pg.display.set_mode(START_SIZE, pg.RESIZABLE)
-pg.event.clear(pg.VIDEORESIZE)
+if ARGS['fullscreen']:
+    pg.display.set_mode(START_SIZE, pg.FULLSCREEN)
+else:
+    pg.display.set_mode(START_SIZE, pg.RESIZABLE)
+    pg.event.clear(pg.VIDEORESIZE)
 
 
 #Resource loading (Fonts and music just contain path names).
@@ -35,4 +42,5 @@ GFX   = tools.load_all_gfx(os.path.join("resources", "graphics"))
 #It's time to start the music, it's time to light the lights
 pg.mixer.music.load(MUSIC["main_stem"])
 pg.mixer.music.set_volume(.2)
-pg.mixer.music.play(-1)
+if not ARGS['music_off']:
+    pg.mixer.music.play(-1)
