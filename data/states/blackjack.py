fbaf903ed78edcd5e37e53d43d1baa6d3cb92dbe
iminurnamez <easinerf@gmail.com>
2014-12-13 12:35:36 -0500

    test from master

--- data/states/blackjack.py
+++ data/states/blackjack.py
@@ -9,7 +9,7 @@ from ..components.blackjack_dealer import Dealer
 from ..components.blackjack_player import Player
 from ..components.blackjack_hand import Hand
 
-some_list = [1,2,3]
+some_list = [1,2,3,4]
 
 class Blackjack(tools._State):
     """State to represent a blackjack game. Player cash
