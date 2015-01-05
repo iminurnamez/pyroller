a0587bcfa91c1a7ac03286c927e7fa8a8f5dae47
Mekire <3ror@live.com>
2015-01-06 02:26:52 +0900

    button prototype

--- data/states/title_screen.py
+++ data/states/title_screen.py
@@ -4,6 +4,7 @@ import pygame as pg
 
 from .. import tools, prepare
 from ..components import spotlight
+from ..components.labels import ButtonGroup
 from ..components.labels import Label, Blinker, MarqueeFrame, NeonButton
 from ..components.casino_player import CasinoPlayer
 from ..components.cards import Deck
@@ -20,7 +21,7 @@ class TitleScreen(tools._State):
         self.screen_rect = pg.Rect((0, 0), prepare.RENDER_SIZE)
         self.marquees = []
         self.make_titles()
-        self.make_buttons()
+        self.buttons = self.make_buttons()
         self.prepare_stats()
         self.make_spotlights()
         self.check_straight_arg()
@@ -76,14 +77,20 @@ class TitleScreen(tools._State):
 
     def make_buttons(self):
         b_width = 318
-        left = self.screen_rect.centerx-(b_width//2)
-        top = self.title2.rect.bottom + 150
-        self.new_game_button = NeonButton((left, top), "New")
-        self.new_game_button.active = False
-        top = self.new_game_button.rect.bottom + 50
-        self.load_game_button = NeonButton((left, top), "Load")
-        self.load_game_button.active = False
-        self.buttons = [self.new_game_button, self.load_game_button]
+        x = self.screen_rect.centerx-(b_width//2)
+        y = self.title2.rect.bottom+150
+        buttons = ButtonGroup()
+        self.new_game_button = NeonButton((x,y), "New", self.load_or_new,
+                                          None, buttons, visible=False)
+        y = self.new_game_button.rect.bottom+50
+        self.load_game_button = NeonButton((x,y), "Load", self.load_or_new,
+                                           True, buttons, visible=False)
+        return buttons
+
+    def load_or_new(self, payload):
+        stats = self.stats if payload else None
+        self.persist["casino_player"] = CasinoPlayer(stats)
+        self.done = True
 
     def get_event(self, event, scale):
         if event.type == pg.QUIT:
@@ -93,14 +100,7 @@ class TitleScreen(tools._State):
             pos = tools.scaled_mouse_pos(scale, event.pos)
             self.title.rect.centerx = self.screen_rect.centerx
             self.title2.rect.centerx = self.screen_rect.centerx
-            if self.new_game_button.rect.collidepoint(pos):
-                if self.new_game_button.active:
-                    self.persist["casino_player"] = CasinoPlayer()
-                    self.done = True
-            elif self.load_game_button.rect.collidepoint(pos):
-                if self.load_game_button.active:
-                    self.persist["casino_player"] = CasinoPlayer(self.stats)
-                    self.done = True
+        self.buttons.get_event(event)
 
     def cleanup(self):
         spotlight.SpotLight.clear_cache() # Clear cache to reclaim memory.
@@ -108,8 +108,7 @@ class TitleScreen(tools._State):
 
     def update(self, surface, keys, current_time, dt, scale):
         mouse_pos = tools.scaled_mouse_pos(scale)
-        for button in self.buttons:
-            button.update(mouse_pos)
+        self.buttons.update(mouse_pos)
         if self.title.rect.centerx < self.screen_rect.centerx:
             self.title.true_x += 0.6*dt
             self.title.rect.x = self.title.true_x
@@ -124,9 +123,9 @@ class TitleScreen(tools._State):
                 for title in (self.title, self.title2):
                     self.marquees.append(MarqueeFrame(title))
                     title.blinking = True
-                self.new_game_button.active = True
+                self.new_game_button.visible = True
                 if self.stats is not None:
-                    self.load_game_button.active = True
+                    self.load_game_button.visible = True
         for marquee in self.marquees:
             marquee.update(dt)
         for light in self.lights:
@@ -141,6 +140,4 @@ class TitleScreen(tools._State):
             marquee.draw(surface)
         for light in self.lights:
             light.draw(surface)
-        for button in self.buttons:
-            if button.active:
-                button.draw(surface)
+        self.buttons.draw(surface)
