8006c85fcfa16f7bce94fa794b125b0fca10b515
Adrian <andarms@outlook.com>
2015-01-04 21:41:17 -0500

    Add video poker machine buttons

--- data/states/video_poker/video_poker_machine.py
+++ data/states/video_poker/video_poker_machine.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from ... import tools, prepare
-from ...components.labels import Blinker, Label
+from ...components.labels import Blinker, Label, Button
 from ...components.cards import Deck
 
 PAYTABLE = [
@@ -123,31 +123,76 @@ class CardsTable:
 class Machine:
     def __init__(self, topleft, size):
         self.rect = pg.Rect(topleft, size)
+        
         self.font = prepare.FONTS["Saniretro"]
+        self.text_size = 30
+        self.text_color = "white"
         self.padding = 25
+
+        self.buttons = []
+        self.btn_width = self.btn_height = 100
+        self.btn_padding = 40
+        self.info_labels = []
+
+        self.credits = 20
+        self.coins = 1
+
         self.build()
+
+
+
+    def test(self):
+        print("Hello world")
+
 
 
     def build(self):
         x, y = self.rect.topleft
         w, h = self.rect.size
 
+        # calculate pay board position
         x += self.padding
         y += self.padding
         w -= self.padding*2 + 150
         h = 330
-
         self.pay_board = PayBoard((x,y), (w,h))
 
-        
+        # calculate cards table position position
         y += self.padding + self.pay_board.rect.h
         h = 300
-
         self.cards_table = CardsTable((x,y), (w,h))
+
+        # game info
+        y += self.padding + self.cards_table.rect.h
+        credit_text = 'Credit {}'.format(self.credits)
+        label = Label(self.font, self.text_size, credit_text, self.text_color, 
+                        {"topright": (w, y)})
+        self.info_labels.append(label)
+        coins_text = "Coins in {}".format(self.coins)
+        label = Label(self.font, self.text_size, coins_text, self.text_color, 
+                        {"topleft": (x, y)})
+        self.info_labels.append(label)
+
+        # buttons
+        y += self.padding + self.btn_padding
+        button_list = ['bet', 'held', 'held', 'held', 'held',
+                        'held', 'bet max', 'draw']
+        for text in button_list:
+            label = Label(self.font, self.text_size, text, self.text_color, {})
+            button = Button(x, y, self.btn_width, self.btn_height, label)
+            self.buttons.append(button)
+            x += self.btn_width + self.btn_padding
+
+
+
 
 
     def draw(self, surface, dt):
         self.pay_board.draw(surface)
         self.cards_table.draw(surface, dt)
+        for label in self.info_labels:
+            label.draw(surface)
+        for button in self.buttons:
+            button.draw(surface)
 
 
