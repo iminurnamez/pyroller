8c8d2446af8e810327435e0a6fedfe6b6f175ef5
Mekire <3ror@live.com>
2015-01-06 06:52:16 +0900

    Added back button to specific stats screens.

--- data/states/stats_screen.py
+++ data/states/stats_screen.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from .. import tools, prepare
-from ..components.labels import Label, GroupLabel, Button, NeonButton
+from ..components.labels import Label, GroupLabel, NeonButton, ButtonGroup
 
 
 class StatsScreen(tools._State):
@@ -10,12 +10,22 @@ class StatsScreen(tools._State):
         super(StatsScreen, self).__init__()
         self.font = prepare.FONTS["Saniretro"]
         screen_rect = pg.Rect((0, 0), prepare.RENDER_SIZE)
-        b_width = 318
-        b_height = 101
-        self.done_button = NeonButton((screen_rect.centerx-(b_width//2),
-                                      screen_rect.bottom - (b_height + 10)),
-                                      "Lobby")
+
+        self.buttons = self.make_buttons(screen_rect)
         self.labels = []
+
+    def make_buttons(self, screen_rect):
+        buttons = ButtonGroup()
+        x,y = (screen_rect.centerx-(NeonButton.width//2)-170,
+               screen_rect.bottom-(NeonButton.height+10))
+        NeonButton((x,y), "Lobby", self.back_to_x, "LOBBYSCREEN", buttons)
+        x = screen_rect.centerx-(NeonButton.width//2)+170
+        NeonButton((x,y), "Back", self.back_to_x, "STATSMENU", buttons)
+        return buttons
+
+    def back_to_x(self, next_state):
+        self.done = True
+        self.next = next_state
 
     def startup(self, current_time, persistent):
         self.start_time = current_time
@@ -25,35 +35,31 @@ class StatsScreen(tools._State):
         stats = self.persist["casino_player"].stats[game]
         self.labels = []
         title = GroupLabel(self.labels, self.font, 64, game, "darkred",
-                                   {"midtop": (screen.centerx, screen.top + 10)})
+                           {"midtop": (screen.centerx, screen.top + 10)})
         left1 = screen.left + 200
         left2 = screen.left + 500
         top = title.rect.bottom + 20
         for stat in stats:
-            label = GroupLabel(self.labels, self.font, 32, stat.title(), "gold3",
-                                         {"topleft": (left1, top)})
-            label2 = GroupLabel(self.labels, self.font, 32, "{}".format(stats[stat]),
-                                           "gold3", {"topleft": (left2, top)})
+            label = GroupLabel(self.labels, self.font, 32, stat.title(),
+                               "gold3", {"topleft": (left1, top)})
+            label2 = GroupLabel(self.labels, self.font, 32,
+                                "{}".format(stats[stat]), "gold3",
+                                {"topleft": (left2, top)})
             top += label.rect.height + 10
 
 
     def get_event(self, event, scale=(1,1)):
         if event.type == pg.QUIT:
-            self.done = True
-            self.next = "LOBBYSCREEN"
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            pos = tools.scaled_mouse_pos(scale, event.pos)
-            if self.done_button.rect.collidepoint(pos):
-                self.done = True
-                self.next = "LOBBYSCREEN"
+            self.back_to_x("LOBBYSCREEN")
+        self.buttons.get_event(event)
 
     def draw(self, surface):
         surface.fill(prepare.BACKGROUND_BASE)
         for label in self.labels:
             label.draw(surface)
-        self.done_button.draw(surface)
+        self.buttons.draw(surface)
 
     def update(self, surface, keys, current_time, dt, scale):
         mouse_pos = tools.scaled_mouse_pos(scale)
-        self.done_button.update(mouse_pos)
+        self.buttons.update(mouse_pos)
         self.draw(surface)
