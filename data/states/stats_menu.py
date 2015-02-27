5ff7b38db0f43742f2cd57747eb6f45319deba67
iminurnamez <easinerf@gmail.com>
2015-02-27 17:14:21 -0500

    Added money overview to stats menu

--- data/states/stats_menu.py
+++ data/states/stats_menu.py
@@ -18,12 +18,38 @@ class StatsMenu(tools._State):
         self.games = ["Blackjack", "Bingo", "Craps", "Keno", "Video Poker",
                       "Pachinko", "Guts"]
         self.buttons = self.make_buttons(screen_rect, 3)
+        self.labels = []
+        self.lines = []
         self.use_music_handler = False
 
+    def make_labels(self):
+        self.labels = []
+        cash = self.player.cash
+        balance = self.player.account_balance
+        assets = cash + balance
+        starting_cash = prepare.MONEY
+        profit = assets - starting_cash
+        label_info = [("Cash", cash, 90),
+                            ("Account", balance, 130),
+                            ("Assets", assets, 178),
+                            ("Starting Cash", -starting_cash, 218),
+                            ("Profit", profit, 263)]
+        left = 500
+        right = 900
+        for name, value, topy in label_info:
+            label1 = Label(self.font, 36, name, "white",
+                                  {"topleft": (left, topy)})
+            color = "darkgreen" if value >= 0 else "darkred"
+            label2 = Label(self.font, 36, "${:.2f}".format(value),
+                                  color, {"topright": (right, topy)})                                           
+            self.labels.extend([label1, label2])
+        self.lines = [((left, 173), (right, 173)),    
+                          ((left, 260), (right, 260))]
+                          
     def make_buttons(self, screen_rect, col=2):
         spacer_x = 20
         spacer_y = 20
-        start_y = self.title.rect.bottom+50
+        start_y = 410
         start_x = (screen_rect.w-NeonButton.width*col-spacer_x*(col-1))//2
         buttons = ButtonGroup()
         for i,game in enumerate(self.games):
@@ -49,6 +75,7 @@ class StatsMenu(tools._State):
         self.start_time = current_time
         self.persist = persistent
         self.player = self.persist["casino_player"]
+        self.make_labels()
 
     def get_event(self, event, scale=(1,1)):
         if event.type == pg.QUIT:
@@ -60,6 +87,10 @@ class StatsMenu(tools._State):
         surface.fill(prepare.BACKGROUND_BASE)
         self.title.draw(surface)
         self.buttons.draw(surface)
+        for label in self.labels:
+            label.draw(surface)
+        for line in self.lines:
+            pg.draw.line(surface, pg.Color("white"), *line)
 
     def update(self, surface, keys, current_time, dt, scale):
         mouse_pos = tools.scaled_mouse_pos(scale)
