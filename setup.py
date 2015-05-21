0cff215ef6306624513633c7a2c4a902c22d27c3
iminurnamez <easinerf@gmail.com>
2015-05-21 07:44:51 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import sys
 from cx_Freeze import setup, Executable
+import pymunk
 
 base = None 
 if sys.platform == "win32":
@@ -13,10 +14,10 @@ includes=[]
 excludes=[]
 packages=[]
 
-setup(version="0.1",
-         description="Py Rollers Casino",
-         author="Erf",
-         name="py_roller",
+setup(version="1.0",
+         description="A Collection of Casino Games",
+         author="PyRollers Collective",
+         name="Pyrollers Casino",
          options={"build_exe": {"includes": includes, "include_files": include_files, "packages": packages, "excludes": excludes}},
          executables=[exe])
 
